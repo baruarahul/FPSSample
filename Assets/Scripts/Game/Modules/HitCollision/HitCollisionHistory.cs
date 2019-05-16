aa04a630ae2a37c9a7ddbd98fb40395ee8e1de3c
Byron Wright <bslayerw@gmail.com>
2019-05-16 12:48:51 -0400

    WIP, added simple tank character.

--- Assets/Scripts/Game/Modules/HitCollision/HitCollisionHistory.cs
+++ Assets/Scripts/Game/Modules/HitCollision/HitCollisionHistory.cs
@@ -26,7 +26,7 @@ public class HitCollisionHistory : MonoBehaviour
     }
 
     public Settings settings;
-    
+
 
     [NonSerialized] public TransformAccessArray collisiderParents;
 
@@ -34,7 +34,7 @@ public class HitCollisionHistory : MonoBehaviour
 #if UNITY_EDITOR    
     private void OnDisable()
     {
-        if(collisiderParents.isCreated)
+        if (collisiderParents.isCreated)
             collisiderParents.Dispose();
     }
 #endif
@@ -44,7 +44,7 @@ public class HitCollisionHistory : MonoBehaviour
 
 public struct HitCollisionData : IComponentData
 {
-    public const int k_maxColliderCount = 16;
+    public const int k_maxColliderCount = 24;
     public const int k_historyCount = 16;
 
     public enum HitCollType
@@ -54,7 +54,7 @@ public struct HitCollisionData : IComponentData
         Head,
     }
 
-    
+
     public struct HitCollInfo
     {
         public HitCollType type;
@@ -71,8 +71,8 @@ public struct HitCollisionData : IComponentData
         public sphere sphere;
     }
 
-    
-    
+
+
     [InternalBufferCapacity(k_maxColliderCount)]
     public struct Box : IBufferElementData
     {
@@ -80,9 +80,9 @@ public struct HitCollisionData : IComponentData
         public int transformIndex;
         public box prim;
     }
-    
+
     [InternalBufferCapacity(k_maxColliderCount)]
-    public struct Capsule: IBufferElementData
+    public struct Capsule : IBufferElementData
     {
         public HitCollInfo info;
         public int transformIndex;
@@ -90,22 +90,22 @@ public struct HitCollisionData : IComponentData
     }
 
     [InternalBufferCapacity(k_maxColliderCount)]
-    public struct Sphere: IBufferElementData
+    public struct Sphere : IBufferElementData
     {
         public HitCollInfo info;
         public int transformIndex;
         public sphere prim;
     }
-    
+
     [InternalBufferCapacity(k_historyCount)]
-    public struct BoundsHistory: IBufferElementData
+    public struct BoundsHistory : IBufferElementData
     {
         public float3 pos;
     }
-    
-    
-    [InternalBufferCapacity(k_maxColliderCount*k_historyCount)]
-    public struct TransformHistory: IBufferElementData
+
+
+    [InternalBufferCapacity(k_maxColliderCount * k_historyCount)]
+    public struct TransformHistory : IBufferElementData
     {
         public float3 pos;
         public quaternion rot;
@@ -120,9 +120,9 @@ public struct HitCollisionData : IComponentData
     public int historyCount;
 
 
-    public static void Setup(EntityManager entityManager, Entity entity, List<Transform> parents, 
-        float boundsRadius, float boundsHeightOffset, List<CapsuleCollider> capsuleColliders, 
-        List<Transform> capsuleColliderParents, List<SphereCollider> sphereColliders, 
+    public static void Setup(EntityManager entityManager, Entity entity, List<Transform> parents,
+        float boundsRadius, float boundsHeightOffset, List<CapsuleCollider> capsuleColliders,
+        List<Transform> capsuleColliderParents, List<SphereCollider> sphereColliders,
         List<Transform> sphereColliderParents, List<BoxCollider> boxColliders, List<Transform> boxColliderParents)
     {
         var coll = new HitCollisionData();
@@ -132,26 +132,26 @@ public struct HitCollisionData : IComponentData
         coll.lastIndex = -1;
         coll.boundsRadius = boundsRadius;
         coll.boundsHeightOffset = boundsHeightOffset;
-        if(entityManager.HasComponent<HitCollisionData>(entity))        
-            entityManager.SetComponentData(entity,coll);
+        if (entityManager.HasComponent<HitCollisionData>(entity))
+            entityManager.SetComponentData(entity, coll);
         else
-            entityManager.AddComponentData(entity,coll);
+            entityManager.AddComponentData(entity, coll);
 
         // Setup history
         entityManager.AddBuffer<TransformHistory>(entity);
         var historyBuffer = entityManager.GetBuffer<TransformHistory>(entity);
-        for (int i = 0; i < k_historyCount*k_maxColliderCount; i++)
+        for (int i = 0; i < k_historyCount * k_maxColliderCount; i++)
         {
-            historyBuffer.Add(new TransformHistory());     
+            historyBuffer.Add(new TransformHistory());
         }
-        
+
         entityManager.AddBuffer<BoundsHistory>(entity);
         var boundsBuffer = entityManager.GetBuffer<BoundsHistory>(entity);
         for (int i = 0; i < k_historyCount; i++)
         {
-            boundsBuffer.Add(new BoundsHistory());     
+            boundsBuffer.Add(new BoundsHistory());
         }
-        
+
 
         // Primitives
         entityManager.AddBuffer<Capsule>(entity);
@@ -163,7 +163,7 @@ public struct HitCollisionData : IComponentData
             var axis = collider.direction == 0 ? Vector3.right :
                 collider.direction == 1 ? Vector3.up : Vector3.forward;
 
-            var offset = 0.5f*axis*(collider.height - 2*collider.radius);
+            var offset = 0.5f * axis * (collider.height - 2 * collider.radius);
             var prim = new capsule()
             {
                 p1 = localPos - offset,
@@ -183,7 +183,7 @@ public struct HitCollisionData : IComponentData
             };
             capsuleBuffer.Add(capsule);
         }
-        
+
         entityManager.AddBuffer<Box>(entity);
         var boxBuffer = entityManager.GetBuffer<Box>(entity);
         for (var i = 0; i < boxColliders.Count; i++)
@@ -199,7 +199,7 @@ public struct HitCollisionData : IComponentData
             var box = new Box();
             box.prim = primlib.transform(prim, collider.transform.localPosition,
                 collider.transform.localRotation);
-            
+
             var parent = boxColliderParents[i];
             box.transformIndex = parents.IndexOf(parent);
             box.info = new HitCollInfo
@@ -223,7 +223,7 @@ public struct HitCollisionData : IComponentData
             var sphere = new Sphere();
             sphere.prim = primlib.transform(prim, collider.transform.localPosition,
                 collider.transform.localRotation);
-            
+
             var parent = sphereColliderParents[i];
             sphere.transformIndex = parents.IndexOf(parent);
             sphere.info = new HitCollInfo
@@ -233,7 +233,7 @@ public struct HitCollisionData : IComponentData
             sphereBuffer.Add(sphere);
         }
     }
-    
+
     public int GetHistoryIndex(int tick)
     {
         // If we exceed buffersize we should always use last value (if player latency to high no rollback is performed)
@@ -246,17 +246,17 @@ public struct HitCollisionData : IComponentData
             index += k_historyCount;
         return index;
     }
-    
 
-    public static bool IsRelevant(EntityManager entityManager, Entity hitCollisionEntity, int flagMask, 
+
+    public static bool IsRelevant(EntityManager entityManager, Entity hitCollisionEntity, int flagMask,
         Entity forceExcluded, Entity forceIncluded)
     {
 
         var hitCollisionData = entityManager.GetComponentData<HitCollisionData>(hitCollisionEntity);
-        
+
         if (hitCollisionData.hitCollisionOwner == Entity.Null)
         {
-            GameDebug.Assert(false,"HitCollisionHistory:" + hitCollisionData + " has a null hitCollisionOwner");
+            GameDebug.Assert(false, "HitCollisionHistory:" + hitCollisionData + " has a null hitCollisionOwner");
             return false;
         }
 
@@ -264,24 +264,24 @@ public struct HitCollisionData : IComponentData
 
         var hitCollisionOwner = entityManager.GetComponentData<HitCollisionOwnerData>(hitCollisionData.hitCollisionOwner);
         var valid = (forceIncluded != Entity.Null && forceIncluded == hitCollisionData.hitCollisionOwner) ||
-                    (hitCollisionOwner.collisionEnabled == 1&&
+                    (hitCollisionOwner.collisionEnabled == 1 &&
                      (hitCollisionOwner.colliderFlags & flagMask) != 0 &&
                      !(forceExcluded != Entity.Null && forceExcluded == hitCollisionData.hitCollisionOwner));
-        
+
         Profiler.EndSample();
-                                                           
+
         return valid;
     }
- 
+
     public static void StoreBones(EntityManager entityManager, Entity entity, TransformAccessArray boneTransformArray, int sampleTick)
     {
         var collData = entityManager.GetComponentData<HitCollisionData>(entity);
-        
+
         var historyBuffer = entityManager.GetBuffer<TransformHistory>(entity);
         var boundsBuffer = entityManager.GetBuffer<BoundsHistory>(entity);
-        
-       
-        
+
+
+
         // To make sure all ticks have valid data we store state of all ticks up to sampleTick that has not been stored (in editor server might run multiple game frames for each time samplestate is called) 
         var lastStoredTick = collData.lastTick;
         var endTick = sampleTick;
@@ -293,8 +293,8 @@ public struct HitCollisionData : IComponentData
             if (collData.historyCount < k_historyCount)
                 collData.historyCount++;
 
-            var slice = new NativeSlice<TransformHistory>(historyBuffer.ToNativeArray(),collData.lastIndex*k_maxColliderCount);
-            
+            var slice = new NativeSlice<TransformHistory>(historyBuffer.ToNativeArray(), collData.lastIndex * k_maxColliderCount);
+
             var job = new StoreBonesJobJob
             {
                 transformBuffer = slice,
@@ -310,89 +310,89 @@ public struct HitCollisionData : IComponentData
 
         entityManager.SetComponentData(entity, collData);
     }
-    
+
     public static void DebugDrawTick(EntityManager entityManager, Entity entity, int tick, Color primColor, Color boundsColor)
     {
-//        var collData = entityManager.GetComponentData<HitCollisionData>(entity);
-//
-//        var histIndex = collData.GetHistoryIndex(tick);
-//        
-//        var historyBuffer = entityManager.GetBuffer<History>(entity);
-//        DebugDraw.Prim(new sphere(historyBuffer[histIndex].center, collData.boundsRadius), boundsColor, 0);
-//
-//        var transformBuffer = entityManager.GetBuffer<TransformHistory>(historyBuffer[histIndex].transformHistory);
-//        
-//        var sphereArray = entityManager.GetBuffer<Sphere>(entity);
-//        for (var i = 0; i < sphereArray.Length; i++)
-//        {
-//            var prim = sphereArray[i].prim;
-//            var sourceIndex = sphereArray[i].transformIndex;                
-//            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
-//            DebugDraw.Prim(prim, primColor, 0);
-//        }
-//
-//        var capsuleArray = entityManager.GetBuffer<Capsule>(entity);
-//        for (var i = 0; i < capsuleArray.Length; i++)
-//        {
-//            var prim = capsuleArray[i].prim;
-//            var sourceIndex = capsuleArray[i].transformIndex;                
-//            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
-//
-//            DebugDraw.Prim(prim, primColor, 0);
-//        }
-//    
-//        var boxArray = entityManager.GetBuffer<Box>(entity);
-//        for (var i = 0; i < boxArray.Length; i++)
-//        {
-//            var prim = boxArray[i].prim;
-//            var sourceIndex = boxArray[i].transformIndex;                
-//            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
-//
-//            DebugDraw.Prim(prim, primColor, 0);
-//        }
+        //        var collData = entityManager.GetComponentData<HitCollisionData>(entity);
+        //
+        //        var histIndex = collData.GetHistoryIndex(tick);
+        //        
+        //        var historyBuffer = entityManager.GetBuffer<History>(entity);
+        //        DebugDraw.Prim(new sphere(historyBuffer[histIndex].center, collData.boundsRadius), boundsColor, 0);
+        //
+        //        var transformBuffer = entityManager.GetBuffer<TransformHistory>(historyBuffer[histIndex].transformHistory);
+        //        
+        //        var sphereArray = entityManager.GetBuffer<Sphere>(entity);
+        //        for (var i = 0; i < sphereArray.Length; i++)
+        //        {
+        //            var prim = sphereArray[i].prim;
+        //            var sourceIndex = sphereArray[i].transformIndex;                
+        //            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
+        //            DebugDraw.Prim(prim, primColor, 0);
+        //        }
+        //
+        //        var capsuleArray = entityManager.GetBuffer<Capsule>(entity);
+        //        for (var i = 0; i < capsuleArray.Length; i++)
+        //        {
+        //            var prim = capsuleArray[i].prim;
+        //            var sourceIndex = capsuleArray[i].transformIndex;                
+        //            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
+        //
+        //            DebugDraw.Prim(prim, primColor, 0);
+        //        }
+        //    
+        //        var boxArray = entityManager.GetBuffer<Box>(entity);
+        //        for (var i = 0; i < boxArray.Length; i++)
+        //        {
+        //            var prim = boxArray[i].prim;
+        //            var sourceIndex = boxArray[i].transformIndex;                
+        //            prim = primlib.transform(prim, transformBuffer[sourceIndex].pos, transformBuffer[sourceIndex].rot);
+        //
+        //            DebugDraw.Prim(prim, primColor, 0);
+        //        }
     }
 
 
-    public static bool SphereOverlapSingle(EntityManager entityManager, Entity entity, int tick, sphere sphere, 
+    public static bool SphereOverlapSingle(EntityManager entityManager, Entity entity, int tick, sphere sphere,
         ref CollisionResult result)
     {
         var collData = entityManager.GetComponentData<HitCollisionData>(entity);
 
         var histIndex = collData.GetHistoryIndex(tick);
         var transformBuffer = entityManager.GetBuffer<TransformHistory>(entity);
-        
+
         var sphereArray = entityManager.GetBuffer<Sphere>(entity);
         var capsuleArray = entityManager.GetBuffer<Capsule>(entity);
         var boxArray = entityManager.GetBuffer<Box>(entity);
-        var resultArray = new NativeArray<CollisionResult>(1,Allocator.TempJob);
+        var resultArray = new NativeArray<CollisionResult>(1, Allocator.TempJob);
 
         var job = new SphereOverlapJob
         {
-            transformBuffer = new NativeSlice<TransformHistory>(transformBuffer.ToNativeArray(),histIndex*k_maxColliderCount),
+            transformBuffer = new NativeSlice<TransformHistory>(transformBuffer.ToNativeArray(), histIndex * k_maxColliderCount),
             sphereArray = sphereArray.ToNativeArray(),
             capsuleArray = capsuleArray.ToNativeArray(),
             boxArray = boxArray.ToNativeArray(),
             sphere = sphere,
             result = resultArray,
         };
-        
+
         var handle = job.Schedule();
         handle.Complete();
         result = resultArray[0];
-        
-        if(math.length(result.box.size) > 0)
+
+        if (math.length(result.box.size) > 0)
             DebugDraw.Prim(result.box, Color.red, 1);
-        if(result.capsule.radius > 0)
+        if (result.capsule.radius > 0)
             DebugDraw.Prim(result.capsule, Color.red, 1);
-        if(result.sphere.radius > 0)
+        if (result.sphere.radius > 0)
             DebugDraw.Prim(result.sphere, Color.red, 1);
-        
-        
+
+
         resultArray.Dispose();
-        
+
         return result.hit == 1;
     }
-    
+
 
 
 
